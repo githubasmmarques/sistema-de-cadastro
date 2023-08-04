03d4a5eee919d446df8c7c5695e9696ef146cc49
011534962267 <alexandre.marques@tre-am.jus.br>
2023-08-04 17:55:32 -0400

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um console log de teste;
 console.log("O Paulo fez esse commit sem você ver na aula")
+console.log("Enviando para o GitHub")
+
+
