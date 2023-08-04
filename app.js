ad2e5e2018c168058e55887a3539dcd4047c755b
011534962267 <alexandre.marques@tre-am.jus.br>
2023-08-04 18:45:56 -0400

    Corrigindo o app js

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um console log de teste;
 console.log("O Paulo fez esse commit sem você ver na aula")
+// teste
